8bbbb60bb978bff49b90cb4b1c0ad90e6a95d97a
Puker228 <karatelanalov@yandex.ru>
2023-12-01 00:44:29 +0300

    lab1

---
+++ lab1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            double c;
+            Console.Write("x = ");
+            x = Convert.ToInt32(Console.ReadLine());
+            c = 2 * (x * x * x * x) - 3 * x * x * x - 5 * x + 6;
+            Console.WriteLine($"C = {c}");
+        }
+    }
+}
