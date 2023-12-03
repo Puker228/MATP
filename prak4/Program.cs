33dc1a63043c14061058a866baef6f711e54171d
Puker228 <karatelanalov@yandex.ru>
2023-12-03 16:13:44 +0300

    a little prak4

--- prak4/Program.cs
+++ prak4/Program.cs
@@ -7,8 +7,11 @@ namespace Prak4
     {
         static void Main()
         {
-            string cont = "yes";
-            while (cont != "no")
+            List<int> llist = new List<int>();
+            int listLen;
+            int listNum;
+            
+            while (true)
             {
                 Console.WriteLine("Выберите дейстивие:");
                 Console.WriteLine("1 - Создать список");
@@ -16,19 +19,43 @@ namespace Prak4
                 Console.WriteLine("3 - Удалить элемент из списка");
                 Console.WriteLine("4 - Вывести список");
                 Console.WriteLine("5 - Уничтожить список");
+                Console.WriteLine("6 - Выйти");
 
 
                 string choice = Console.ReadLine();
                 switch (choice)
                 {
                     case "1":
-                        List<int> llist = new List<int>();
-                        Console.WriteLine("Создан новый список\n");
+                        Console.Write("Количество элементов в списке: ");
+                        listLen = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 1; i <= listLen; i++)
+                        {
+                            Console.Write($"\n{i} элемент: ");
+                            llist.Add(Convert.ToInt32(Console.ReadLine()));
+                        }
+                        Console.WriteLine("\nСоздан новый список\n");
                         break;
+                    case "2":
+                        Console.WriteLine("Какой элемент вы хотите заменить?");
+                        listNum = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Число:");
+                        llist[listNum] = Convert.ToInt32(Console.ReadLine());
+                        
+                        break;
+                    case "4" :
+                        foreach (var i in llist)
+                        {
+                            Console.Write($"{i} ");
+                        }
+                        Console.WriteLine();
+                        break;
+                    case "5":
+                        llist.Clear();
+                        Console.WriteLine("\nСписок взорван\n");
+                        break;
+                    case "6":
+                        return;
                 }
-                
-                
-                cont = Console.ReadLine();
             }
         }
     }
