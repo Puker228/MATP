9000dfad97237d850855b2b5dc9ddbacd52e5a9b
Puker228 <karatelanalov@yandex.ru>
2023-12-12 23:33:19 +0300

    prak4.2

--- LinkedListExample/Program.cs
+++ LinkedListExample/Program.cs
@@ -1,227 +1,154 @@
 ﻿using System;
-using System.Text;
-using System.Collections.Generic;
 
-public class Example
+namespace prak4_2
 {
-    public static void Main()
+    class Program
     {
-        // Create the link list.
-        string[] words =
-            { "the", "fox", "jumps", "over", "the", "dog" };
-        LinkedList<string> sentence = new LinkedList<string>(words);
-        Display(sentence, "The linked list values:");
-        Console.WriteLine("sentence.Contains(\"jumps\") = {0}",
-            sentence.Contains("jumps"));
-
-        // Add the word 'today' to the beginning of the linked list.
-        sentence.AddFirst("today");
-        Display(sentence, "Test 1: Add 'today' to beginning of the list:");
-
-        // Move the first node to be the last node.
-        LinkedListNode<string> mark1 = sentence.First;
-        sentence.RemoveFirst();
-        sentence.AddLast(mark1);
-        Display(sentence, "Test 2: Move first node to be last node:");
-
-        // Change the last node to 'yesterday'.
-        sentence.RemoveLast();
-        sentence.AddLast("yesterday");
-        Display(sentence, "Test 3: Change the last node to 'yesterday':");
-
-        // Move the last node to be the first node.
-        mark1 = sentence.Last;
-        sentence.RemoveLast();
-        sentence.AddFirst(mark1);
-        Display(sentence, "Test 4: Move last node to be first node:");
-
-        // Indicate the last occurence of 'the'.
-        sentence.RemoveFirst();
-        LinkedListNode<string> current = sentence.FindLast("the");
-        IndicateNode(current, "Test 5: Indicate last occurence of 'the':");
-
-        // Add 'lazy' and 'old' after 'the' (the LinkedListNode named current).
-        sentence.AddAfter(current, "old");
-        sentence.AddAfter(current, "lazy");
-        IndicateNode(current, "Test 6: Add 'lazy' and 'old' after 'the':");
-
-        // Indicate 'fox' node.
-        current = sentence.Find("fox");
-        IndicateNode(current, "Test 7: Indicate the 'fox' node:");
-
-        // Add 'quick' and 'brown' before 'fox':
-        sentence.AddBefore(current, "quick");
-        sentence.AddBefore(current, "brown");
-        IndicateNode(current, "Test 8: Add 'quick' and 'brown' before 'fox':");
-
-        // Keep a reference to the current node, 'fox',
-        // and to the previous node in the list. Indicate the 'dog' node.
-        mark1 = current;
-        LinkedListNode<string> mark2 = current.Previous;
-        current = sentence.Find("dog");
-        IndicateNode(current, "Test 9: Indicate the 'dog' node:");
-
-        // The AddBefore method throws an InvalidOperationException
-        // if you try to add a node that already belongs to a list.
-        Console.WriteLine("Test 10: Throw exception by adding node (fox) already in the list:");
-        try
+        static void Main()
         {
-            sentence.AddBefore(current, mark1);
+            DoublyLinkedList list = null;
+            int choice;
+            List<int> sub_list = new List<int>();
+            
+            do
+            {
+                Console.WriteLine("1 - Создать список");
+                Console.WriteLine("2 - Добавить в конец списка");
+                Console.WriteLine("3 - Добавить в произвольное место");
+                Console.WriteLine("4 - Удалить элемент из списка");
+                Console.WriteLine("5 - Вывести список");
+                Console.WriteLine("6 - Уничтожить список");
+                Console.WriteLine("7 - Выйти");
+                
+                Console.Write("Действие: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        list = new DoublyLinkedList();
+                        Console.WriteLine("Список создан");
+                        break;
+                    case 2:
+                        Console.Write("Новые данные: ");
+                        int data = Convert.ToInt32(Console.ReadLine());
+                        list.AddToEnd(data);
+                        break;
+                    case 3:
+                        Console.Write("Новые данные: ");
+                        int insertData = Convert.ToInt32(Console.ReadLine());
+                        Console.Write("Позиция: ");
+                        int position = Convert.ToInt32(Console.ReadLine());
+                        list.InsertAt(insertData, position);
+                        break;
+                    case 4:
+                        Console.Write("Позиция: ");
+                        int deletePosition = Convert.ToInt32(Console.ReadLine());
+                        list.Delete(deletePosition);
+                        break;
+                    case 5:
+                        list.Read();
+                        break;
+                    case 6:
+                        list = null;
+                        break;
+                    case 7:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Нет такого выбора");
+                        break;
+                }
+            } while (choice != 7);
         }
-        catch (InvalidOperationException ex)
-        {
-            Console.WriteLine("Exception message: {0}", ex.Message);
-        }
-        Console.WriteLine();
-
-        // Remove the node referred to by mark1, and then add it
-        // before the node referred to by current.
-        // Indicate the node referred to by current.
-        sentence.Remove(mark1);
-        sentence.AddBefore(current, mark1);
-        IndicateNode(current, "Test 11: Move a referenced node (fox) before the current node (dog):");
-
-        // Remove the node referred to by current.
-        sentence.Remove(current);
-        IndicateNode(current, "Test 12: Remove current node (dog) and attempt to indicate it:");
-
-        // Add the node after the node referred to by mark2.
-        sentence.AddAfter(mark2, current);
-        IndicateNode(current, "Test 13: Add node removed in test 11 after a referenced node (brown):");
-
-        // The Remove method finds and removes the
-        // first node that that has the specified value.
-        sentence.Remove("old");
-        Display(sentence, "Test 14: Remove node that has the value 'old':");
-
-        // When the linked list is cast to ICollection(Of String),
-        // the Add method adds a node to the end of the list.
-        sentence.RemoveLast();
-        ICollection<string> icoll = sentence;
-        icoll.Add("rhinoceros");
-        Display(sentence, "Test 15: Remove last node, cast to ICollection, and add 'rhinoceros':");
-
-        Console.WriteLine("Test 16: Copy the list to an array:");
-        // Create an array with the same number of
-        // elements as the linked list.
-        string[] sArray = new string[sentence.Count];
-        sentence.CopyTo(sArray, 0);
-
-        foreach (string s in sArray)
-        {
-            Console.WriteLine(s);
-        }
-
-        // Release all the nodes.
-        sentence.Clear();
-
-        Console.WriteLine();
-        Console.WriteLine("Test 17: Clear linked list. Contains 'jumps' = {0}",
-            sentence.Contains("jumps"));
-
     }
 
-    private static void Display(LinkedList<string> words, string test)
+    class Node
     {
-        Console.WriteLine(test);
-        foreach (string word in words)
-        {
-            Console.Write(word + " ");
-        }
-        Console.WriteLine();
-        Console.WriteLine();
+        public int data;
+        public Node prev;
+        public Node next;
     }
 
-    private static void IndicateNode(LinkedListNode<string> node, string test)
+    class DoublyLinkedList
     {
-        Console.WriteLine(test);
-        if (node.List == null)
+        private Node head;
+
+        public void AddToEnd(int data)
         {
-            Console.WriteLine("Node '{0}' is not in the list.\n",
-                node.Value);
-            return;
+            Node newNode = new Node();
+            newNode.data = data;
+
+            if (head == null)
+            {
+                head = newNode;
+            }
+            else
+            {
+                Node lastNode = head;
+                while (lastNode.next != null)
+                {
+                    lastNode = lastNode.next;
+                }
+                lastNode.next = newNode;
+                newNode.prev = lastNode;
+            }
         }
 
-        StringBuilder result = new StringBuilder("(" + node.Value + ")");
-        LinkedListNode<string> nodeP = node.Previous;
-
-        while (nodeP != null)
+        public void InsertAt(int data, int position)
         {
-            result.Insert(0, nodeP.Value + " ");
-            nodeP = nodeP.Previous;
+            Node newNode = new Node();
+            newNode.data = data;
+
+            if (position == 1)
+            {
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode;
+            }
+            else
+            {
+                Node temp = head;
+                for (int i = 1; i < position - 1; i++)
+                {
+                    temp = temp.next;
+                }
+
+                newNode.next = temp.next;
+                temp.next.prev = newNode;
+                temp.next = newNode;
+                newNode.prev = temp;
+            }
         }
 
-        node = node.Next;
-        while (node != null)
+        public void Delete(int position)
         {
-            result.Append(" " + node.Value);
-            node = node.Next;
+            if (position == 1)
+            {
+                head = head.next;
+                head.prev = null;
+            }
+            else
+            {
+                Node temp = head;
+                for (int i = 1; i < position - 1; i++)
+                {
+                    temp = temp.next;
+                }
+                temp.next = temp.next.next;
+                temp.next.prev = temp;
+            }
         }
 
-        Console.WriteLine(result);
+        public void Read()
+        {
+            Node current = head;
+            while (current != null)
+            {
+                Console.Write(current.data + " ");
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
     }
 }
-
-//This code example produces the following output:
-//
-//The linked list values:
-//the fox jumps over the dog
-
-//Test 1: Add 'today' to beginning of the list:
-//today the fox jumps over the dog
-
-//Test 2: Move first node to be last node:
-//the fox jumps over the dog today
-
-//Test 3: Change the last node to 'yesterday':
-//the fox jumps over the dog yesterday
-
-//Test 4: Move last node to be first node:
-//yesterday the fox jumps over the dog
-
-//Test 5: Indicate last occurence of 'the':
-//the fox jumps over (the) dog
-
-//Test 6: Add 'lazy' and 'old' after 'the':
-//the fox jumps over (the) lazy old dog
-
-//Test 7: Indicate the 'fox' node:
-//the (fox) jumps over the lazy old dog
-
-//Test 8: Add 'quick' and 'brown' before 'fox':
-//the quick brown (fox) jumps over the lazy old dog
-
-//Test 9: Indicate the 'dog' node:
-//the quick brown fox jumps over the lazy old (dog)
-
-//Test 10: Throw exception by adding node (fox) already in the list:
-//Exception message: The LinkedList node belongs a LinkedList.
-
-//Test 11: Move a referenced node (fox) before the current node (dog):
-//the quick brown jumps over the lazy old fox (dog)
-
-//Test 12: Remove current node (dog) and attempt to indicate it:
-//Node 'dog' is not in the list.
-
-//Test 13: Add node removed in test 11 after a referenced node (brown):
-//the quick brown (dog) jumps over the lazy old fox
-
-//Test 14: Remove node that has the value 'old':
-//the quick brown dog jumps over the lazy fox
-
-//Test 15: Remove last node, cast to ICollection, and add 'rhinoceros':
-//the quick brown dog jumps over the lazy rhinoceros
-
-//Test 16: Copy the list to an array:
-//the
-//quick
-//brown
-//dog
-//jumps
-//over
-//the
-//lazy
-//rhinoceros
-
-//Test 17: Clear linked list. Contains 'jumps' = False
-//
--- prak4.2/Program.cs
+++ prak4.2/Program.cs
@@ -6,7 +6,69 @@ namespace prak4_2
     {
         static void Main()
         {
-            Console.WriteLine(228);
+            List<int> sub_list = new List<int>();
+            LinkedList<int> main_list = new LinkedList<int>();
+
+            while (true)
+            {
+                Console.WriteLine("Выберите дейстивие:");
+                Console.WriteLine("1 - Создать список");
+                Console.WriteLine("2 - Добавить элемент в список(2)");
+                Console.WriteLine("3 - Удалить элемент из списка");
+                Console.WriteLine("4 - Вывести список");
+                Console.WriteLine("5 - Уничтожить список");
+                Console.WriteLine("6 - Выйти");
+
+                Console.Write("Действие: ");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Количество элементов в списке: ");
+                        int listLen = Convert.ToInt32(Console.ReadLine());
+                        for (int i = 1; i <= listLen; i++)
+                        {
+                            Console.Write($"\n{i} элемент ");
+                            sub_list.Add(Convert.ToInt32(Console.ReadLine()));
+                        }
+
+                        main_list = new LinkedList<int>(sub_list);
+                        Console.WriteLine("\nСоздан новый список");
+                        break;
+                    case 2:
+                        Console.WriteLine("1 - Добавить элемент в произвольное место");
+                        Console.WriteLine("2 - Добавить элемент в конец списка");
+                        Console.Write("Действие: ");
+
+                        int inChoice = Convert.ToInt32(Console.ReadLine());
+                        switch (inChoice)
+                        {
+                            case 1:
+                                Console.Write("Новое значение: ");
+                                int new_data = Convert.ToInt32(Console.ReadLine());
+                                Console.WriteLine("Позиция: ");
+                                int pos = Convert.ToInt32(Console.ReadLine());
+                                break;
+                        }
+
+                        break;
+                    case 4:
+                        foreach (var i in main_list)
+                        {
+                            Console.Write($"{i}");
+                        }
+
+                        Console.WriteLine();
+                        break;
+                    case 5:
+                        main_list.Clear();
+                        Console.WriteLine("Список обнулен");
+                        break;
+                    case 6:
+                        return;
+                }
+            }
         }
     }
 }
