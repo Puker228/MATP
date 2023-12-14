6da8de60ff60a92a9e2f07748c594ed4e7815083
Puker228 <karatelanalov@yandex.ru>
2023-12-14 20:23:04 +0300

    another lab8 fix

--- lab8/Fraction.cs
+++ lab8/Fraction.cs
@@ -4,7 +4,7 @@ public class Fraction
 {
     private int numerator;
     private int denominator;
-
+    
     public Fraction(int numerator, int denominator)
     {
         if (denominator == 0)
@@ -15,35 +15,55 @@ public class Fraction
         this.numerator = numerator;
         this.denominator = denominator;
     }
-
+    
     public Fraction Add(Fraction fraction)
     {
-        int newNumerator = numerator * fraction.denominator + fraction.numerator * fraction.denominator;
+        int newNumerator = numerator * fraction.denominator + fraction.numerator * denominator;
         int newDenominator = denominator * fraction.denominator;
-        return new Fraction(newNumerator, newDenominator);
+        return simple(new Fraction(newNumerator, newDenominator));
     }
 
     public Fraction Sub(Fraction fraction)
     {
-        int newNumerator = numerator * fraction.denominator - fraction.numerator * fraction.denominator;
+        int newNumerator = numerator * fraction.denominator - fraction.numerator * denominator;
         int newDenominator = denominator * fraction.denominator;
-        return new Fraction(newNumerator, newDenominator);
+        return simple(new Fraction(newNumerator, newDenominator));
     }
 
     public Fraction Mult(Fraction fraction)
     {
         int newNumerator = numerator * fraction.numerator;
         int newDenominator = denominator * fraction.denominator;
-        return new Fraction(newNumerator, newDenominator);
+        return simple(new Fraction(newNumerator, newDenominator));
     }
 
     public Fraction Div(Fraction fraction)
     {
         int newNumerator = numerator * fraction.denominator;
         int newDenominator = denominator * fraction.numerator;
-        return new Fraction(newNumerator, newDenominator);
+        return simple(new Fraction(newNumerator, newDenominator));
     }
+    
+    private Fraction simple(Fraction fraction)
+    {
+        int gcd = GCD(fraction.numerator, fraction.denominator);
+        return new Fraction(fraction.numerator / gcd, fraction.denominator / gcd);
+    }
+    
+    private int GCD(int a, int b)
+    {
+        a = Math.Abs(a);
+        b = Math.Abs(b);
+        while (b != 0)
+        {
+            int t = b;
+            b = a % b;
+            a = t;
+        }
 
+        return a;
+    }
+    
     public override string ToString()
     {
         return numerator + "/" + denominator;
